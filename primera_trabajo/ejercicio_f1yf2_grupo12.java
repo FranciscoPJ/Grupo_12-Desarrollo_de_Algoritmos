8bf5cd6de755e84439d467d25378114a1ca13e6f
FranciscoPJ <francisco.pandolfi@est.fi.uncoma.edu.ar>
2026-08-30 18:42:38 -0300

    Primer trabajo grupal

---
+++ primera_trabajo/ejercicio_f1yf2_grupo12.java
@@ -0,0 +1,8 @@
+package tp_grupal.primera_trabajo;
+import java.util.Scanner;
+
+public class ejercicio_f1yf2_grupo12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
